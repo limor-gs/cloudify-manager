f4656e0c9fbd4df8774e7bea80a7b241caef9a5b
Łukasz Maksymczuk <lukasz@lukasz-maksymczuk.intra.codilime.com>
2018-07-24 11:09:10 +0200

    Queries

--- amqp-postgres/amqp_postgres/postgres_publisher.py
+++ amqp-postgres/amqp_postgres/postgres_publisher.py
@@ -15,19 +15,69 @@
 ############
 
 import Queue
-import psycopg2
-from uuid import uuid4
 from time import time
 from threading import Thread, Lock
-from collections import OrderedDict, namedtuple
 
-from manager_rest.storage.models import Event, Log, Execution
-
-Exec = namedtuple('Execution', 'storage_id creator_id tenant_id')
+import psycopg2
+from psycopg2.extras import execute_batch
 
 
-LOG_INSERT_QUERY = Log.__table__.insert()
-EVENT_INSERT_QUERY = Event.__table__.insert()
+EVENT_INSERT_QUERY = """
+    INSERT INTO events (
+        timestamp,
+        reported_timestamp,
+        _execution_fk,
+        _tenant_id,
+        _creator_id,
+        event_type,
+        message,
+        message_code,
+        operation,
+        node_id,
+        error_causes)
+    SELECT
+        now() AT TIME ZONE 'utc',
+        CAST (%s AS TIMESTAMP),
+        _storage_id,
+        _tenant_id,
+        _creator_id,
+        %s,
+        %s,
+        %s,
+        NULLIF(%s, ''),
+        NULLIF(%s, ''),
+        NULLIF(%s, '')
+    FROM executions WHERE id = %s
+"""
+
+LOG_INSERT_QUERY = """
+    INSERT INTO logs (
+        timestamp,
+        reported_timestamp,
+        _execution_fk,
+        _tenant_id,
+        _creator_id,
+        logger,
+        level,
+        message,
+        message_code,
+        operation,
+        node_id)
+    SELECT
+        now() AT TIME ZONE 'utc',
+        CAST (%s AS TIMESTAMP),
+        _storage_id,
+        _tenant_id,
+        _creator_id,
+        %s,
+        %s,
+        %s,
+        %s,
+        NULLIF(%s, ''),
+        NULLIF(%s, '')
+    FROM executions
+    WHERE id = %s
+"""
 
 
 class DBLogEventPublisher(object):
@@ -39,7 +89,6 @@ class DBLogEventPublisher(object):
 
         self._last_commit = time()
         self.config = config
-        self._executions_cache = LimitedSizeDict(10000)
 
         # Create a separate thread to allow proper batching without losing
         # messages. Without this thread, if the messages were just committed,
@@ -50,8 +99,7 @@ class DBLogEventPublisher(object):
         publish_thread.start()
 
     def process(self, message, exchange):
-        execution = self._get_current_execution(message)
-        item = self._get_item(message, exchange, execution)
+        item = self._get_item(message, exchange)
         self._batch.put(item)
 
     def _message_publisher(self):
@@ -73,88 +121,48 @@ class DBLogEventPublisher(object):
             if len(events) > 100 or \
                     (events and (time() - self._last_commit > 0.5)):
                 with conn.cursor() as cur:
-                    cur.execute_batch(EVENT_INSERT_QUERY, events)
+                    execute_batch(cur, EVENT_INSERT_QUERY, events)
                 conn.commit()
                 events = []
                 self._last_commit = time()
             if len(logs) > 100 or \
                     (logs and (time() - self._last_commit > 0.5)):
                 with conn.cursor() as cur:
-                    cur.execute_batch(LOG_INSERT_QUERY, logs)
+                    execute_batch(cur, LOG_INSERT_QUERY, logs)
                 conn.commit()
                 logs = []
                 self._last_commit = time()
 
-    def _get_current_execution(self, message):
-        """ Return execution from cache if exists, or from DB if needed """
-
-        execution_id = message['context']['execution_id']
-        execution = self._executions_cache.get(execution_id)
-        if not execution:
-            db_execution = Execution.query.filter_by(id=execution_id).first()
-            execution = Exec(
-                storage_id=db_execution._storage_id,
-                creator_id=db_execution._creator_id,
-                tenant_id=db_execution._tenant_id
-            )
-            self._executions_cache[execution_id] = execution
-        return execution
-
-    def _get_item(self, message, exchange, execution):
+    def _get_item(self, message, exchange):
         if exchange == 'cloudify-events':
-            return 'event', self._get_event(message, execution)
+            return 'event', self._get_event(message)
         elif exchange == 'cloudify-logs':
-            return 'log', self._get_log(message, execution)
+            return 'log', self._get_log(message)
         else:
             raise ValueError('Unknown exchange type: {0}'.format(exchange))
 
     @staticmethod
-    def _get_log(message, execution):
-        return dict(
-            id=str(uuid4()),
-            reported_timestamp=message['timestamp'],
-            logger=message['logger'],
-            level=message['level'],
-            message=message['message']['text'],
-            operation=message['context'].get('operation'),
-            node_id=message['context'].get('node_id'),
-            _execution_fk=execution.storage_id,
-            _tenant_id=execution.tenant_id,
-            _creator_id=execution.creator_id
+    def _get_log(message):
+        return (
+            message['timestamp'],
+            message['logger'],
+            message['level'],
+            message['message']['text'],
+            message['message_code'],
+            message['context']['operation'],
+            message['context']['node_id'],
+            message['context']['execution_id']
         )
 
     @staticmethod
-    def _get_event(message, execution):
-        return dict(
-            id=str(uuid4()),
-            reported_timestamp=message['timestamp'],
-            event_type=message['event_type'],
-            message=message['message']['text'],
-            operation=message['context'].get('operation'),
-            node_id=message['context'].get('node_id'),
-            error_causes=message['context'].get('task_error_causes'),
-            _execution_fk=execution.storage_id,
-            _tenant_id=execution.tenant_id,
-            _creator_id=execution.creator_id
+    def _get_event(message):
+        return (
+            message['timestamp'],
+            message['event_type'],
+            message['message']['text'],
+            message['message_code'],
+            message['context']['operation'],
+            message['context']['node_id'],
+            message['context']['task_error_causes'],
+            message['context']['execution_id']
         )
-
-
-class LimitedSizeDict(OrderedDict):
-    """
-    A FIFO dictionary with a maximum size limit. If number of keys reaches
-    the limit, the elements added first will be popped
-    Implementation taken from https://stackoverflow.com/a/2437645/978089
-    """
-    def __init__(self, size_limit=None, *args, **kwds):
-        self.size_limit = size_limit
-        OrderedDict.__init__(self, *args, **kwds)
-        self._check_size_limit()
-
-    def __setitem__(self, key, value):
-        OrderedDict.__setitem__(self, key, value)
-        self._check_size_limit()
-
-    def _check_size_limit(self):
-        if self.size_limit is not None:
-            while len(self) > self.size_limit:
-                self.popitem(last=False)
